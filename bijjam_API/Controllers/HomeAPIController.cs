e18cadb096fbc0970722acb6d58a2be4879b9e41
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-04-04 13:24:22 +0530

    hi

---
+++ bijjam_API/Controllers/HomeAPIController.cs
@@ -0,0 +1,47 @@
+﻿using bijjam_API.Data;
+using bijjam_API.Model;
+using bijjam_API.Model.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bijjam_API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/HomeAPI")]
+    [ApiController]
+
+    public class HomeAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult< IEnumerable<HomeDTO>> GetHomes() 
+        {
+            return Ok(HomeStore.HomeList);
+       
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200, Type =typeof(HomeDTO))]
+   
+        public ActionResult<HomeDTO> GetHome(int id) 
+        {
+            if (id == 0) 
+            {
+                return BadRequest();
+            
+            }
+            var Home = HomeStore.HomeList.FirstOrDefault(u => u.Id == id);
+            if (Home == null)
+            { 
+                return NotFound();  
+            }
+
+            return Ok(Home);
+        }
+
+
+
+    }
+}
