2fbb5b421b6271f1941c0e24b82c5019f3188611
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-04-07 15:02:56 +0530

    Add Parameter in Id1

--- bijjam_API/Data/HomeStore.cs
+++ bijjam_API/Data/HomeStore.cs
@@ -5,7 +5,7 @@ namespace bijjam_API.Data
     public  static class HomeStore
     {
        public static List<HomeDTO> HomeList = new List<HomeDTO> {
-            new HomeDTO { Id = 1, Name ="Pool View"},
+            new HomeDTO { Id = 1, Name ="Pool View",Sqft =150, Occupancy =9},
 
             new HomeDTO { Id = 2, Name = "Beach View",Sqft =100, Occupancy =4 },
             new HomeDTO { Id = 3, Name = "Green View" ,Sqft =300, Occupancy =5}
