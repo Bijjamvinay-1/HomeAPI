91a8ad811e2940d8b77fd35e46c97c6264997fe0
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-04-19 18:26:55 +0530

    sample

--- bijjam_API/Data/ApplicationDbContext.cs
+++ bijjam_API/Data/ApplicationDbContext.cs
@@ -10,75 +10,75 @@ namespace bijjam_API.Data
         } 
          public DbSet<Home>Homes { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Home>().HasData(
-        //       new Home
-        //       {
-        //           Id = 1,
-        //           Name = "Royal Home",
-        //           Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-        //           ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg",
-        //           Occupancy = 4,
-        //           Rate = 200,
-        //           Sqft = 550,
-        //           Amenity = "",
-        //           CreatedDate = DateTime.Now,
-        //       },
-        //      new Home
-        //      {
-        //          Id = 2,
-        //          Name = "Premium Pool Home",
-        //          Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-        //          ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg",
-        //          Occupancy = 4,
-        //          Rate = 300,
-        //          Sqft = 550,
-        //          Amenity = "",
-        //          CreatedDate = DateTime.Now,
-        //      },
-        //      new Home
-        //      {
-        //          Id = 3,
-        //          Name = "Luxury Pool Home",
-        //          Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-        //          ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg",
-        //          Occupancy = 4,
-        //          Rate = 400,
-        //          Sqft = 750,
-        //          Amenity = "",
-        //          CreatedDate = DateTime.Now,
-        //      },
-        //      new Home
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Home>().HasData(
+               new Home
+               {
+                   Id = 1,
+                   Name = "Royal Home",
+                   Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                   ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg",
+                   Occupancy = 4,
+                   Rate = 200,
+                   Sqft = 550,
+                   Amenity = "",
+                   CreatedDate = DateTime.Now,
+               },
+              new Home
+              {
+                  Id = 2,
+                  Name = "Premium Pool Home",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg",
+                  Occupancy = 4,
+                  Rate = 300,
+                  Sqft = 550,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now,
+              },
+              new Home
+              {
+                  Id = 3,
+                  Name = "Luxury Pool Home",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg",
+                  Occupancy = 4,
+                  Rate = 400,
+                  Sqft = 750,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now,
+              },
+              new Home
               
-        //      {
-        //          Id = 4,
-        //          Name = "Diamond Home",
-        //          Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-        //          ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa5.jpg",
-        //          Occupancy = 4,
-        //          Rate = 550,
-        //          Sqft = 900,
-        //          Amenity = "",
-        //          CreatedDate = DateTime.Now,   
-        //      },
-        //      new Home
+              {
+                  Id = 4,
+                  Name = "Diamond Home",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa5.jpg",
+                  Occupancy = 4,
+                  Rate = 550,
+                  Sqft = 900,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now,   
+              },
+              new Home
 
-        //      {
-        //          Id = 5,
-        //          Name = "Diamond Pool Home",
-        //          Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-        //          ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa2.jpg",
-        //          Occupancy = 4,
-        //          Rate = 600,
-        //          Sqft = 1100,
-        //          Amenity = "",
-        //          CreatedDate = DateTime.Now,
-        //      }
+              {
+                  Id = 5,
+                  Name = "Diamond Pool Home",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa2.jpg",
+                  Occupancy = 4,
+                  Rate = 600,
+                  Sqft = 1100,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now,
+              }
              
 
-        //        );
-        //}
+                );
+        }
 
     }
 }
